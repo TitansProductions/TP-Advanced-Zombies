100f8c0b59707a710df10a655e4fff5a9264e9f3
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2022-08-05 13:47:27 +0300

    Add files via upload

--- tp-advancedzombies/server/tp-server_main.lua
+++ tp-advancedzombies/server/tp-server_main.lua
@@ -1,7 +1,5 @@
 ESX, QBCore      = nil, nil
 
-players = {}
-
 
 if Config.Framework == "ESX" then
     TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
@@ -9,24 +7,8 @@ elseif Config.Framework == "QBCore" then
     QBCore = exports['qb-core']:GetCoreObject()
 end
 
-AddEventHandler("onResourceStop", function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
-        return
-    end
-
-    players = nil
-end)
-
 AddEventHandler("playerDropped", function(reason)
-    players[source] = nil
     TriggerClientEvent('tp-advancedzombies:clearZombies', source)
-    TriggerClientEvent("tp-advancedzombies:onPlayerUpdate", -1, players)
-end)
-
-RegisterServerEvent("tp-advancedzombies:onNewPlayerId")
-AddEventHandler("tp-advancedzombies:onNewPlayerId", function(id)
-    players[source] = id
-    TriggerClientEvent("tp-advancedzombies:onPlayerUpdate", -1, players)
 end)
 
 
