a7a9a709d56d1194bd9679a4a72558ea8300dc3e
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2022-08-06 00:25:02 +0300

    Add files via upload

--- tp-advancedzombies/server/tp-server_main.lua
+++ tp-advancedzombies/server/tp-server_main.lua
@@ -11,6 +11,11 @@ AddEventHandler("playerDropped", function(reason)
     TriggerClientEvent('tp-advancedzombies:clearZombies', source)
 end)
 
+RegisterServerEvent("tp-advancedzombies:getZombieEntityOnServer")
+AddEventHandler("tp-advancedzombies:getZombieEntityOnServer", function(data)
+	TriggerClientEvent("tp-advancedzombies:getZombieEntityOnClient", source, data)
+end)
+
 
 RegisterServerEvent("tp-advancedzombies:onZombieSpawningStart")
 AddEventHandler("tp-advancedzombies:onZombieSpawningStart", function()
