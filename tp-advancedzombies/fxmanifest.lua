1b6e1671b2914eac7c50cff2a61022976c4b5e91
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2022-09-18 12:02:32 +0300

    Update fxmanifest.lua

--- tp-advancedzombies/fxmanifest.lua
+++ tp-advancedzombies/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Nosmakos'
 description 'Titans Productions Advanced Zombies System (Supporting ESX, QBCore & Standalone)'
-version '1.6.3'
+version '1.6.4'
 
 ui_page 'html/index.html'
 
